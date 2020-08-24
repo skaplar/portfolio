4f737b0e5dd74a1fe1ba4ad1730eba4a0b006c39
Sebastijan Kaplar <sebastijan.kaplar@gmail.com>
2020-08-24 22:33:27 +0200

    Building simple User and signup functionalities.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
 
+  # Sessions allowed
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  # Models manipulation
+  resources :users
   resources :tag_aliases
   resources :tags
-  get 'home/index'
-
   resources :portfolio
   resources :articles do
     resources :notes, only: [:index, :new, :create, :destroy]
   end
 
+  get 'home/index'
   root 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_104741) do
+ActiveRecord::Schema.define(version: 2020_08_24_201309) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,17 @@ ActiveRecord::Schema.define(version: 2020_08_22_104741) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first"
+    t.string "last"
+    t.boolean "admin", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
