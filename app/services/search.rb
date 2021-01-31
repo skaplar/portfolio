ac349b21c84ee1cc035b1244e5a05850ade196b5
Sebastijan Kaplar <sebastijan.kaplar@gmail.com>
2021-01-31 15:03:21 +0100

    Improving search a bit.

--- app/services/search.rb
+++ app/services/search.rb
@@ -8,7 +8,11 @@ module Search
     def find
       if(@params.has_key?(:query))
         query_param = @params[:query].downcase
-        return Article.all.where("lower(title) LIKE :query", query: query_param)
+        articles_titles = Article.all.where("lower(title) LIKE :query", query: "%#{query_param}%")
+        articles_tags = Article.joins(:tags).where("lower(tags.name) = ?", query_param)
+        tags = Tag.joins(:tag_aliases).where("lower(tag_aliases.alias) = ?", query_param)
+        articles_aliases = Article.joins(:tags).where("tags.id = ?", tags.ids)
+        return articles_titles + articles_tags + articles_aliases
       else
         return Article.all
       end
